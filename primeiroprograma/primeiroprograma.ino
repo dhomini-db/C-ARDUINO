8da477715e4e0b3240d5525c6024e081d92e1d6f
Dhomini De Bom <dbomdhomini@gmail.com>
2026-03-16 23:00:19 -0300

    Add files via upload

---
+++ primeiroprograma/primeiroprograma.ino
@@ -0,0 +1,13 @@
+int ledamarelo = 2;
+
+void setup() {
+pinMode(ledamarelo,OUTPUT);
+
+}
+void loop() {
+digitalWrite(ledamarelo,HIGH);
+delay(1000);
+digitalWrite(ledamarelo,LOW);
+delay(1000);
+
+}
